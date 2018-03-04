51265656d6341b24261d3a1aa9e587d06da65a58
Bart Langelaan <langelaanbart@gmail.com>
2018-03-05 00:19:29 +0100

    refactor: update gatsby-node.js to es6

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,76 +1,57 @@
 const path = require('path');
-const _ = require('lodash');
+const { createFilePath } = require('gatsby-source-filesystem');
 
 exports.onCreateNode = ({ node, boundActionCreators, getNode }) => {
   const { createNodeField } = boundActionCreators;
-  let slug;
   if (node.internal.type === 'MarkdownRemark') {
-    const fileNode = getNode(node.parent);
-    const parsedFilePath = path.parse(fileNode.relativePath);
-    if (
-      Object.prototype.hasOwnProperty.call(node, 'frontmatter') &&
-      Object.prototype.hasOwnProperty.call(node.frontmatter, 'slug')
-    ) {
-      slug = `/${_.kebabCase(node.frontmatter.slug)}`;
-    }
-    if (
-      Object.prototype.hasOwnProperty.call(node, 'frontmatter') &&
-      Object.prototype.hasOwnProperty.call(node.frontmatter, 'title')
-    ) {
-      slug = `/${_.kebabCase(node.frontmatter.title)}`;
-    } else if (parsedFilePath.name !== 'index' && parsedFilePath.dir !== '') {
-      slug = `/${parsedFilePath.dir}/${parsedFilePath.name}/`;
-    } else if (parsedFilePath.dir === '') {
-      slug = `/${parsedFilePath.name}/`;
-    } else {
-      slug = `/${parsedFilePath.dir}/`;
-    }
-    createNodeField({ node, name: 'slug', value: slug });
+    const slug = createFilePath({
+      node,
+      getNode,
+      basePath: 'lessons',
+      trailingSlash: false,
+    });
+
+    createNodeField({
+      node,
+      name: 'slug',
+      value: slug,
+    });
   }
 };
 
-exports.createPages = ({ graphql, boundActionCreators }) => {
+exports.createPages = async ({ graphql, boundActionCreators }) => {
   const { createPage } = boundActionCreators;
-
-  return new Promise((resolve, reject) => {
-    const lessonPage = path.resolve('src/templates/lesson.jsx');
-    resolve(
-      graphql(
-        `
-          {
-            allMarkdownRemark {
-              edges {
-                node {
-                  frontmatter {
-                    tags
-                    category
-                    type
-                  }
-                  fields {
-                    slug
-                  }
-                }
-              }
+  const lessonPage = path.resolve('src/templates/lesson.jsx');
+  const result = await graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              tags
+              category
+              type
+            }
+            fields {
+              slug
             }
           }
-        `,
-      ).then(result => {
-        if (result.errors) {
-          /* eslint no-console: "off" */
-          console.log(result.errors);
-          reject(result.errors);
         }
+      }
+    }
+  `);
 
-        result.data.allMarkdownRemark.edges.forEach(edge => {
-          createPage({
-            path: edge.node.fields.slug,
-            component: lessonPage,
-            context: {
-              slug: edge.node.fields.slug,
-            },
-          });
-        });
-      }),
-    );
+  if (result.errors) {
+    throw new Error(result.errors);
+  }
+
+  result.data.allMarkdownRemark.edges.forEach(edge => {
+    createPage({
+      path: edge.node.fields.slug,
+      component: lessonPage,
+      context: {
+        slug: edge.node.fields.slug,
+      },
+    });
   });
 };
