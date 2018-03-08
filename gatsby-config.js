775bf49dcb2cfef8c7fcd5a03a7b979b0be2c17f
Bart Langelaan <langelaanbart@gmail.com>
2018-03-08 20:09:10 +0100

    fix: remove siteConfig dependency from gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,4 @@
-const config = require('./data/SiteConfig');
-
-const pathPrefix = config.pathPrefix === '/' ? '' : config.pathPrefix;
-
 module.exports = {
-  pathPrefix: config.pathPrefix,
-  siteMetadata: {
-    siteUrl: config.siteUrl + pathPrefix,
-  },
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
@@ -49,24 +41,8 @@ module.exports = {
       resolve: 'gatsby-plugin-manifest',
       options: {
         name: 'Headless Ninja',
-        short_name: 'Headless Ninja',
-        description: config.siteDescription,
-        start_url: config.pathPrefix,
-        background_color: config.backgroundColor,
-        theme_color: config.themeColor,
+        start_url: '/',
         display: 'minimal-ui',
-        icons: [
-          {
-            src: '/logos/logo-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/logos/logo-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
       },
     },
     'gatsby-plugin-offline',
