83b249e3a4cabc6411221657a5528edb0fdab5e2
Bart Langelaan <langelaanbart@gmail.com>
2018-03-09 15:38:01 +0100

    feat: change meta description

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from 'styled-components';
 import './css/index.css';
 import './css/prism-okaidia.css';
 import theme from './theme';
+import site from '../../content/site.yml';
 
 export default class MainLayout extends React.Component {
   render() {
@@ -11,10 +12,7 @@ export default class MainLayout extends React.Component {
     return (
       <div>
         <Helmet>
-          <meta
-            name="description"
-            content="Headless Ninja combines the best of Drupal and React."
-          />
+          <meta name="description" content={site.description} />
         </Helmet>
         <ThemeProvider theme={theme}>{children()}</ThemeProvider>
       </div>
