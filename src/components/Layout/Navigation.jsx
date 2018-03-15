38f4090f9da2692440077a68b8b8da602225c61d
Bart Langelaan <langelaanbart@gmail.com>
2018-03-15 12:49:26 +0100

    refactor: simplify Navigation

--- src/components/Layout/Navigation.jsx
+++ src/components/Layout/Navigation.jsx
@@ -2,30 +2,29 @@ import React from 'react';
 import Link from 'gatsby-link';
 import styled from 'styled-components';
 
-const NavContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  width: 100%;
+const NavContainer = styled.nav`
+  text-align: left;
   background: ${props => props.theme.brand};
 
-  .nav-link {
-    font-size: 1.6rem;
-    margin-right: 10px;
-    font-weight: 200;
-    color: black;
+  @media screen and (max-width: 600px) {
+    text-align: center;
   }
 
-  @media screen and (max-width: 600px) {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-
-    section {
-      margin-bottom: 20px;
-    }
-
-    span {
-      display: none;
+  a {
+    color: black;
+    background-image: none;
+    text-shadow: none;
+    padding: 0 10px 7px;
+    margin: 0 20px;
+    &:hover {
+      background-image: linear-gradient(
+        to top,
+        rgba(0, 0, 0, 0),
+        rgba(0, 0, 0, 0) 1px,
+        black 1px,
+        black 2px,
+        rgba(0, 0, 0, 0) 2px
+      );
     }
   }
 `;
@@ -34,20 +33,9 @@ class Navigation extends React.Component {
   render() {
     return (
       <NavContainer>
-        <section>
-          <Link className="nav-link" to="/">
-            {' '}
-            HOME{' '}
-          </Link>
-          <Link className="nav-link" to="/docs">
-            {' '}
-            DOCS{' '}
-          </Link>
-          <Link className="nav-link" to="/about">
-            {' '}
-            ABOUT{' '}
-          </Link>
-        </section>
+        <Link to="/">HOME</Link>
+        <Link to="/docs">DOCS</Link>
+        <Link to="/about">ABOUT</Link>
       </NavContainer>
     );
   }
