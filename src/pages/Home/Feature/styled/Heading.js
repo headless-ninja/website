d220348cd9f6dd74e97068b4f3470ef20dc08926
Ian Wensink <ian@burst-digital.com>
2018-04-05 00:43:13 +0200

    fix(home): add hover animation to features section

--- src/pages/Home/Feature/styled/Heading.js
+++ src/pages/Home/Feature/styled/Heading.js
@@ -8,6 +8,8 @@ export default styled.h3`
   font-weight: 300;
   margin: 0 0 20px !important;
   position: relative;
+  display: inline-block;
+  padding-bottom: 5px;
 
   &::before {
     content: '';
@@ -22,6 +24,26 @@ export default styled.h3`
 
     @media screen and (min-width: 768px) {
       left: -27px;
+    }
+  }
+
+  &::after {
+    content: '';
+    background: #fff;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 0;
+    height: 2px;
+    z-index: 0;
+    border-radius: 100px;
+    transition: width 0.4s ease;
+    opacity: 0.7;
+  }
+
+  &:hover {
+    &::after {
+      width: 100%;
     }
   }
 `;
