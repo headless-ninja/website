402487b5dc6da6b320f8a7a2149b1f3bef7a8f9f
Headless Ninja Bot (from Travis CI) <github@headless.ninja>
2018-04-04 18:58:21 +0000

    Deploy headless.ninja to github.com/headless-ninja/website.git:gh-pages

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-e4c617fd5c5bb9f52952.js","343d045cd6d02c581030fe763e213d77"],["/commons-462399814b4e007fd5fa.js","d93794800c27dd09c9da2b83d2bc7ab0"],["/index.html","eb8ef6527aada0a1dc0f09ab602179f7"],["/manifest.json","1df848db6b2de6787238063b73885462"],["/offline-plugin-app-shell-fallback/index.html","e04d2fe5a35764aa408a3b2c7713b23f"]];
+var precacheConfig = [["/app-16defca85ce37841a373.js","43fe1173b17eed8226a57cc3f463afaa"],["/commons-462399814b4e007fd5fa.js","d93794800c27dd09c9da2b83d2bc7ab0"],["/index.html","63dfce09475d7ff15d2b66140dde1500"],["/manifest.json","1df848db6b2de6787238063b73885462"],["/offline-plugin-app-shell-fallback/index.html","68949a38c708943a5f1a0855ab19210a"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
